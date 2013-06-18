96cfceca7672da3b3bc3ee8f9be8716466b531ff
Pankaj Kumar Jha <pankajjha.tech@gmail.com>
2013-06-18 13:40:48 +0530

    Create Google_Map_With_Multiple_Marker_And_Popup.php

---
+++ Google_Map_With_Multiple_Marker_And_Popup.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html> 
+<head> 
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8" /> 
+  <title>Google Maps Multiple Markers</title> 
+  <script src="http://maps.google.com/maps/api/js?sensor=false" 
+          type="text/javascript"></script>
+</head> 
+<body>
+  <div id="map" style="width: 500px; height: 400px;"></div>
+
+  <script type="text/javascript">
+    var locations = [
+      ['111 Congress Avenue, Ste. 180', 30.263979,-97.744482, 4],
+      ['600 Congress Avenue, Ste. G-230',  30.268468,-97.743123, 5],
+      ['700 Lavaca Street, Ste. 110',  30.270298,-97.745254, 3]      
+    ];
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 14,
+      center: new google.maps.LatLng(30.26, -97.74),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {  
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
+  </script>
+</body>
+</html>
