2e06750d3cddcd5dc69145b7beb5893fda0e09e7
Pankaj Kumar Jha <pankajjha.tech@gmail.com>
2013-06-18 13:39:29 +0530

    Create Google_Map_With_Direction.php

---
+++ Google_Map_With_Direction.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="initial-scale=1.0, user-scalable=no"/>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+<title>Google Maps JavaScript API v3 Example: Directions Simple</title>
+
+<style>
+html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+#map_canvas {
+  height: 100%;
+}
+
+@media print {
+  html, body {
+    height: auto;
+  }
+
+  #map_canvas {
+    height: 650px;
+  }
+}
+
+</style>
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
+<script type="text/javascript">
+  var directionDisplay;
+  var directionsService = new google.maps.DirectionsService();
+  var map;
+
+  function initialize() {
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    var chicago = new google.maps.LatLng(30.270299, -97.745244);
+    var myOptions = {
+      zoom:7,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      center: chicago
+    }
+    map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+    directionsDisplay.setMap(map);
+    calcRoute();
+  }
+  
+  function calcRoute() {
+    //var start = document.getElementById("start").value;
+    //var end = document.getElementById("end").value;
+    var start = "600 Congress Avenue, Ste. G-230, Austin";
+    var end = "700 Lavaca Street, Ste. 110, Austin";
+    
+    var request = {
+        origin:start, 
+        destination:end,
+        travelMode: google.maps.DirectionsTravelMode.DRIVING
+    };
+    directionsService.route(request, function(response, status) {
+      if (status == google.maps.DirectionsStatus.OK) {
+        directionsDisplay.setDirections(response);
+      }
+    });
+  }
+</script>
+</head>
+<body onload="initialize()">
+<div id="map_canvas" style="top:30px;"></div>
+</body>
+</html>
